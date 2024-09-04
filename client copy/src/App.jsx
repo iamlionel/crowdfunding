493f1176d6abd48e0ffcc4f679c375bbb9fb9f4e
lionel-imac <flf241892@sina.cn>
2024-09-04 19:42:33 +0800

    client copy

---
+++ client copy/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { CampaignDetails, CreateCampaign, Home, Profile } from "./pages";
+import { Navbar, Sidebar } from "./components";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
